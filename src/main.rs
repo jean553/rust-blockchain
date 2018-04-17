8019c9c4b8abe23e9734d8afafcc826723f4f792
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-17 14:00:08 +0200

    wip create new block

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 extern crate time;
 extern crate sha1;
+extern crate bincode;
 
+#[macro_use] extern crate serde;
+#[macro_use] extern crate serde_derive;
+
+#[derive(Serialize)]
 struct Block {
-    timestamp: time::Tm,
+    timestamp: i64,
     data: i32,
-    previous: Option<sha1::Sha1>,
+    previous: Option<[u8; 20]>,
 }
 
 fn main() {
 
     /* generate the genesis block */
+
     let chain = Block {
-        timestamp: time::now_utc(),
+        timestamp: time::now_utc().to_timespec().sec,
         data: 0,
         previous: None,
     };
 
     println!("Genesis block has been generated.");
+
+    /* add one block to the ledger: serialize the previous object into raw bytes,
+       in order to generate a new hash digest from those bytes,
+       and use that digest as a previous field content for the new block */
+
+    let previous_block_bytes = bincode::serialize(&chain).unwrap();
+
+    let chain = Block {
+        timestamp: time::now_utc().to_timespec().sec,
+        data: 10,
+        previous: Some(sha1::Sha1::from(previous_block_bytes).digest().bytes()),
+    };
 }
