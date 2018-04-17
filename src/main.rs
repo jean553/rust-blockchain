3e38a18c0cdbb8f5b6aa19a0d1fb921c31719fff
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-17 23:16:15 +0200

    define specific function to generate the block digest

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,20 @@ impl Blockchain {
         data: i32,
     ) {
 
-        let bytes = bincode::serialize(&self).unwrap();
-        let digest = sha1::Sha1::from(bytes).digest().bytes();
-
         self.timestamp = get_current_timestamp();
         self.data = data;
-        self.previous = digest;
+        self.previous = self.get_digest().bytes();
+    }
+
+    /// Returns the hash digest of the current block.
+    ///
+    /// Returns:
+    ///
+    /// sha1 digest of the current block
+    fn get_digest(&self) -> sha1::Digest {
+
+        let bytes = bincode::serialize(&self).unwrap();
+        sha1::Sha1::from(bytes).digest()
     }
 }
 
@@ -89,6 +97,7 @@ fn main() {
             chain.add_block(data);
 
             println!("One block has been added to the ledger.");
+            println!("Current block digest: {}", chain.get_digest());
         }
     }
 }
