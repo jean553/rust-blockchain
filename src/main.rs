aa4043a3e14691bb6c0928b1e12d093e6e2b42b6
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-17 13:10:10 +0200

    generate the genesis block

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,17 @@ extern crate sha1;
 struct Block {
     timestamp: time::Tm,
     data: i32,
-    previous: sha1::Sha1,
+    previous: Option<sha1::Sha1>,
 }
 
 fn main() {
+
+    /* generate the genesis block */
+    let chain = Block {
+        timestamp: time::now_utc(),
+        data: 0,
+        previous: None,
+    };
+
+    println!("Genesis block has been generated.");
 }
