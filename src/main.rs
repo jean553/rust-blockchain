14701b790efd061afe02096073b55d70f8b91533
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-17 20:17:08 +0200

    add a block to the ledger

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ extern crate time;
 extern crate sha1;
 extern crate bincode;
 
-#[macro_use] extern crate serde;
+extern crate serde;
 #[macro_use] extern crate serde_derive;
 
 #[derive(Serialize)]
 struct Block {
     timestamp: i64,
     data: i32,
-    previous: Option<[u8; 20]>,
+    previous: [u8; 20],
 }
 
 fn main() {
@@ -19,7 +19,7 @@ fn main() {
     let chain = Block {
         timestamp: time::now_utc().to_timespec().sec,
         data: 0,
-        previous: None,
+        previous: [0; 20],
     };
 
     println!("Genesis block has been generated.");
@@ -28,11 +28,14 @@ fn main() {
        in order to generate a new hash digest from those bytes,
        and use that digest as a previous field content for the new block */
 
-    let previous_block_bytes = bincode::serialize(&chain).unwrap();
+    let previous_bytes = bincode::serialize(&chain).unwrap();
+    let previous_digest = sha1::Sha1::from(previous_bytes).digest().bytes();
 
-    let chain = Block {
+    let _chain = Block {
         timestamp: time::now_utc().to_timespec().sec,
         data: 10,
-        previous: Some(sha1::Sha1::from(previous_block_bytes).digest().bytes()),
+        previous: previous_digest,
     };
+
+    println!("One block has been added to the ledger.");
 }
