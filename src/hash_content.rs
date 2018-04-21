a40a669ec951986b9369522725e5861d25b934b9
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-21 20:58:37 +0200

    create the hash content module

---
+++ src/hash_content.rs
@@ -0,0 +1,46 @@
+//! Hashed content of every chain block.
+
+use time;
+
+#[derive(Serialize, Deserialize)]
+pub struct HashContent {
+    timestamp: i64,
+    data: i32,
+}
+
+impl HashContent {
+
+    /// Creates a brand new hash content.
+    ///
+    /// Args:
+    ///
+    /// `data` - the data to store into the block hash content
+    ///
+    /// Returns:
+    ///
+    /// hash content with current timestamp and given data
+    pub fn new(data: i32) -> HashContent {
+        HashContent {
+            timestamp: time::now_utc().to_timespec().sec,
+            data: data,
+        }
+    }
+
+    /// Getter of the timestamp.
+    ///
+    /// Returns:
+    ///
+    /// block creation timestamp
+    pub fn get_timestamp(&self) -> i64 {
+        self.timestamp
+    }
+
+    /// Getter of the data.
+    ///
+    /// Returns:
+    ///
+    /// block data
+    pub fn get_data(&self) -> i32 {
+        self.data
+    }
+}
