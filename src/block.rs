28a42d48de999d07e4021db19bfa0e93770ce91b
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-21 21:05:58 +0200

    create the block module

---
+++ src/block.rs
@@ -0,0 +1,61 @@
+//! A chain block.
+
+use bincode::serialize;
+use sha1::Sha1;
+
+use hash_content::HashContent;
+
+#[derive(Serialize, Deserialize)]
+pub struct Block {
+    content: HashContent,
+    previous: String,
+    current: String,
+}
+
+impl Block {
+
+    /// One block constructor. Creates the block from the given data and previous digest. Calculates its own hash digest.
+    ///
+    /// Args:
+    ///
+    /// `data` - the data of the block
+    /// `previous` - the digest of the previous block (empty if genesis)
+    ///
+    /// Returns:
+    ///
+    /// new block
+    pub fn new(
+        data: i32,
+        previous: String,
+    ) -> Block {
+
+        let content = HashContent::new(data);
+        let bytes = serialize(&content).unwrap();
+        let digest = Sha1::from(bytes).hexdigest();
+
+        Block {
+            content: content,
+            previous: previous,
+            current: digest,
+        }
+    }
+
+    /// Getter of the current block hash digest.
+    ///
+    /// Returns:
+    ///
+    /// current block digest as string
+    pub fn get_current(&self) -> &str {
+        &self.current
+    }
+
+    /// Getter of the hashed content.
+    ///
+    /// Returns:
+    ///
+    /// block hashed content
+    pub fn get_content(&self) -> &HashContent {
+        &self.content
+    }
+}
+
