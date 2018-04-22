70c1a5703cd0d7c9da930b861f2ced7e2e103177
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-22 20:14:46 +0200

    refactor the help command result into a dedicated module

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ mod hash_content;
 mod block;
 mod blocks;
 mod peers;
+mod help;
 
 use block::Block;
 
@@ -46,6 +47,8 @@ use peers::{
     create_peer,
     list_peers,
 };
+
+use help::list_commands;
 
 const DEFAULT_STATUS: &str = "Waiting. Type 'help' to get the commands list.";
 
@@ -255,17 +258,7 @@ fn main() {
             list_peers(&peers);
         }
         else if command == HELP {
-
-            /* TODO: should use command options */
-
-            println!("add_block [data] - append a block into the local blockchain");
-            println!("Example: add_block 10 \n");
-            println!("send [ip] - send a copy of the blockchain to another node");
-            println!("Example: send 172.17.0.10\n");
-            println!("receive - receive a copy of the blockchain from another node\n");
-            println!("list - list the local chain blocks\n");
-            println!("add_peer - add one node as a peer");
-            println!("Example: add_peer 172.17.0.10");
+            list_commands();
         }
     }
 }
