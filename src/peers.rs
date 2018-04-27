f07914a3881f3676e954142d17ad457cb4e2fffd
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-27 13:03:21 +0200

    get latest block from peer just after peer insertion

--- src/peers.rs
+++ src/peers.rs
@@ -1,6 +1,10 @@
 //! Peers routines.
 
-use std::net::SocketAddr;
+use std::net::{
+    SocketAddr,
+    TcpStream,
+};
+use std::time::Duration;
 use std::str::FromStr;
 
 /// Creates a new peer.
@@ -14,13 +18,29 @@ pub fn create_peer(peers: &mut Vec<SocketAddr>, address: &str) {
     const PORT: &str = "10000";
     let full_address = format!("{}:{}", address, PORT);
 
-    match SocketAddr::from_str(&full_address) {
+    let socket_address = match SocketAddr::from_str(&full_address) {
         Ok(socket_address) => {
-            peers.push(socket_address);
             println!("Address {} added to peers list.", address);
+            socket_address
         },
         Err(_) => {
             println!("Incorrect address format.");
+            return;
+        }
+    };
+
+    peers.push(socket_address.clone());
+
+    println!("Connecting to {}...", address);
+
+    let mut stream = match TcpStream::connect_timeout(
+        &socket_address,
+        Duration::from_secs(5),
+    ) {
+        Ok(stream) => stream,
+        Err(_) => {
+            println!("The peer {} has been added but cannot be joined right now.", address);
+            return;
         }
     };
 }
